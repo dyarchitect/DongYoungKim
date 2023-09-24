5afbb8301d8881f86521c4f24fd26c9b394efc40
MinboyKim <kdm3801@gmail.com>
2023-09-25 00:11:00 +0900

    feat: kdy ver2.0
    대규모 업데이트

--- banner.js
+++
@@ -1,7 +0,0 @@
-const homeIcon = document.querySelector(".title_home");
-const banner = document.querySelector(".banner");
-
-homeIcon.addEventListener("click", (event) => {
-  event.preventDefault();
-  banner.classList.toggle("hidden");
-});
